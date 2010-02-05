66325abaa0e73169190b199dc2774b8edd43b3c0
Brandon Mechtley <bmechtley+github@gmail.com>
2010-02-05 08:29:41 -0700

    changes to work with exceptions

--- examples/segmentation.cpp
+++ examples/segmentation.cpp
@@ -35,126 +35,124 @@ int main(int argc, char** argv) {
 		 *----------------*/
 		cout << "1: Loading sound: " << argv[1] << endl;
 		
-		Sound* sound = new Sound();
-		sound->setFrameLength(0.04);
-		sound->setHopLength(0.02);
-		sound->open(argv[1]);
-	
-		cout << "\tDuration: " << sound->getSampleCount() / sound->getChannels() << " samples/channel (" 
-			<< sound->getFrameCount() << " frames, " << 
-			sound->getSampleCount() / (sound->getSampleRate() * sound->getChannels()) << "s)" << endl;
-		cout << "\tChannels: " << sound->getChannels() << endl;
-		cout << "\tSample rate: " << sound->getSampleRate() << " Hz" << endl;
-		cout << "\tFrame size: " << sound->getSamplesPerFrame() / sound->getChannels() << " samples/channel (" << sound->getFrameLength() << "s)" << endl;
-		cout << "\tHop size: " << sound->getSamplesPerHop() / sound->getChannels() << " samples/channel (" << sound->getHopLength() << "s)" << endl;
-		cout << "\tSpectrum size: " << sound->getSpectrumSize() << " bins" << endl;
+		Sound sound;
+		sound.setFrameLength(0.04);
+		sound.setHopLength(0.02);
 		
-		/*--------------------------------------*
-		 * 2. Initialize segmentation features. *
-		 *--------------------------------------*/
-		cout << "2. Initializing feature parameters." << endl;
-		
-		Loudness* loudness = new Loudness(sound->getFrameCount());
-		loudness->setMin(-60);
-		loudness->setMax(0);
-		loudness->getSegmentationParameters()->setAlpha(0.15);
-		loudness->getSegmentationParameters()->setR(0.0098);
-		loudness->getSegmentationParameters()->setCStayOff(0.0015);
-		loudness->getSegmentationParameters()->setCTurnOn(0.085);
-		loudness->getSegmentationParameters()->setCTurnOff(0.085);
-		loudness->getSegmentationParameters()->setCNewSegment(0.085);
-		loudness->getSegmentationParameters()->setCStayOn(0.05);
-		loudness->getSegmentationParameters()->setPLagPlus(0.75);
-		loudness->getSegmentationParameters()->setPLagMinus(0.75);
-	
-		SpectralCentroid* spectral_centroid = new SpectralCentroid(sound->getFrameCount(), sound->getSpectrumSize(), sound->getSampleRate());
-		spectral_centroid->setMin(0.4994);
-		spectral_centroid->setMax(25.7848);
-		spectral_centroid->getSegmentationParameters()->setAlpha(0.05);
-		spectral_centroid->getSegmentationParameters()->setR(0.00000196);
-		spectral_centroid->getSegmentationParameters()->setCStayOff(0.0000933506);
-		spectral_centroid->getSegmentationParameters()->setCTurnOn(0.85);
-		spectral_centroid->getSegmentationParameters()->setCTurnOff(0.85);
-		spectral_centroid->getSegmentationParameters()->setCNewSegment(0.85);
-		spectral_centroid->getSegmentationParameters()->setCStayOn(0.0025296018);
-		spectral_centroid->getSegmentationParameters()->setPLagPlus(0.75);
-		spectral_centroid->getSegmentationParameters()->setPLagMinus(0.075);
-	
-		SpectralSparsity* spectral_sparsity = new SpectralSparsity(sound->getFrameCount());
-		spectral_sparsity->setMin(0);
-		spectral_sparsity->setMax(0.6509);
-		spectral_sparsity->getSegmentationParameters()->setAlpha(0.05);
-		spectral_sparsity->getSegmentationParameters()->setR(0.0196);
-		spectral_sparsity->getSegmentationParameters()->setCStayOff(0.001833506);
-		spectral_sparsity->getSegmentationParameters()->setCTurnOn(0.85);
-		spectral_sparsity->getSegmentationParameters()->setCTurnOff(0.85);
-		spectral_sparsity->getSegmentationParameters()->setCNewSegment(0.85);
-		spectral_sparsity->getSegmentationParameters()->setCStayOn(0.009296018);
-		spectral_sparsity->getSegmentationParameters()->setPLagPlus(0.75);
-		spectral_sparsity->getSegmentationParameters()->setPLagMinus(0.75);
-	
-		FeatureSet* feature_set = new FeatureSet();
-		feature_set->addSampleFeature(loudness);
-		feature_set->addSpectralFeature(spectral_centroid);
-		feature_set->addSpectralFeature(spectral_sparsity);
-	
-		/*----------------------------------*
-		 * 3. Extract feature trajectories. *
-		 *----------------------------------*/
-		cout << "3. Extracting features from sound." << endl;
-		
-		sound->setFeatureSet(feature_set);
-		sound->extractFeatures();	
-		
-		/*--------------------------------*
-		 * 4. Get the segment boundaries. *
-		 *--------------------------------*/
-		cout << "4. Segmenting sound." << endl;
-		
-		Segmenter* segmenter = new Segmenter(0.00000000001, 0.00000000001);
-		segmenter->setFeatureSet(feature_set);
-		segmenter->segment();
-		
-		vector<vector<int> > segments = segmenter->getSegments();
-		vector<int> modes = segmenter->getModes();
-		
-		if (segments.size() > 0) {
-			cout << "\tSegments ([start frame]-[end frame]): " << endl;
+		try {
+			sound.open(argv[1]);
 			
-			for (int i = 0; i < segments.size(); i++)
-				cout << "\t\t" << i << ": " << segments[i][0] << "-" << segments[i][1] << endl;
-		} else
-			cout << "\tNo segments." << endl;
-		
-		/*-----------------------------------------------------------------*
-		 * 5. Reload the sound and cut it up, saving the segments to disk. *
-		 *-----------------------------------------------------------------*/
-		cout << "5. Saving segments to disk." << endl;
-		
-		if (segments.size() < 1)
-			cout << "\tNo segments to save." << endl;
-		else {
-			cout << "\tSaving " << segments.size() << " segments." << endl;
+			cout << "\tDuration: " << sound.getSampleCount() / sound.getChannels() << " samples/channel (" 
+				<< sound.getFrameCount() << " frames, " << 
+				sound.getSampleCount() / (sound.getSampleRate() * sound.getChannels()) << "s)" << endl;
+			cout << "\tChannels: " << sound.getChannels() << endl;
+			cout << "\tSample rate: " << sound.getSampleRate() << " Hz" << endl;
+			cout << "\tFrame size: " << sound.getSamplesPerFrame() / sound.getChannels() << " samples/channel (" << sound.getFrameLength() << "s)" << endl;
+			cout << "\tHop size: " << sound.getSamplesPerHop() / sound.getChannels() << " samples/channel (" << sound.getHopLength() << "s)" << endl;
+			cout << "\tSpectrum size: " << sound.getSpectrumSize() << " bins" << endl;
 			
-			for (int i = 0; i < segments.size(); i++) {
-				cout << "\t\tSaving segment " << i << "." << endl;
-				sound->saveSegment("segment" + double_to_string(i) + ".wav", segments[i][0], segments[i][1]);
+			/*--------------------------------------*
+			 * 2. Initialize segmentation features. *
+			 *--------------------------------------*/
+			cout << "2. Initializing feature parameters." << endl;
+			
+			Loudness loudness(sound.getFrameCount());
+			loudness.setMin(-60);
+			loudness.setMax(0);
+			loudness.getSegmentationParameters()->setAlpha(0.15);
+			loudness.getSegmentationParameters()->setR(0.0098);
+			loudness.getSegmentationParameters()->setCStayOff(0.0015);
+			loudness.getSegmentationParameters()->setCTurnOn(0.085);
+			loudness.getSegmentationParameters()->setCTurnOff(0.085);
+			loudness.getSegmentationParameters()->setCNewSegment(0.085);
+			loudness.getSegmentationParameters()->setCStayOn(0.05);
+			loudness.getSegmentationParameters()->setPLagPlus(0.75);
+			loudness.getSegmentationParameters()->setPLagMinus(0.75);
+			
+			SpectralCentroid spectral_centroid(sound.getFrameCount(), sound.getSpectrumSize(), sound.getSampleRate());
+			spectral_centroid.setMin(0.4994);
+			spectral_centroid.setMax(25.7848);
+			spectral_centroid.getSegmentationParameters()->setAlpha(0.05);
+			spectral_centroid.getSegmentationParameters()->setR(0.00000196);
+			spectral_centroid.getSegmentationParameters()->setCStayOff(0.0000933506);
+			spectral_centroid.getSegmentationParameters()->setCTurnOn(0.85);
+			spectral_centroid.getSegmentationParameters()->setCTurnOff(0.85);
+			spectral_centroid.getSegmentationParameters()->setCNewSegment(0.85);
+			spectral_centroid.getSegmentationParameters()->setCStayOn(0.0025296018);
+			spectral_centroid.getSegmentationParameters()->setPLagPlus(0.75);
+			spectral_centroid.getSegmentationParameters()->setPLagMinus(0.075);
+			
+			SpectralSparsity spectral_sparsity(sound.getFrameCount());
+			spectral_sparsity.setMin(0);
+			spectral_sparsity.setMax(0.6509);
+			spectral_sparsity.getSegmentationParameters()->setAlpha(0.05);
+			spectral_sparsity.getSegmentationParameters()->setR(0.0196);
+			spectral_sparsity.getSegmentationParameters()->setCStayOff(0.001833506);
+			spectral_sparsity.getSegmentationParameters()->setCTurnOn(0.85);
+			spectral_sparsity.getSegmentationParameters()->setCTurnOff(0.85);
+			spectral_sparsity.getSegmentationParameters()->setCNewSegment(0.85);
+			spectral_sparsity.getSegmentationParameters()->setCStayOn(0.009296018);
+			spectral_sparsity.getSegmentationParameters()->setPLagPlus(0.75);
+			spectral_sparsity.getSegmentationParameters()->setPLagMinus(0.75);
+			
+			FeatureSet feature_set;
+			feature_set.addSampleFeature(&loudness);
+			feature_set.addSpectralFeature(&spectral_centroid);
+			feature_set.addSpectralFeature(&spectral_sparsity);
+			
+			/*----------------------------------*
+			 * 3. Extract feature trajectories. *
+			 *----------------------------------*/
+			cout << "3. Extracting features from sound." << endl;
+			
+			sound.setFeatureSet(&feature_set);
+			sound.extractFeatures();	
+			
+			/*--------------------------------*
+			 * 4. Get the segment boundaries. *
+			 *--------------------------------*/
+			cout << "4. Segmenting sound." << endl;
+			
+			Segmenter segmenter(0.00000000001, 0.00000000001);
+			segmenter.setFeatureSet(&feature_set);
+			segmenter.segment();
+			
+			vector<vector<int> > segments = segmenter.getSegments();
+			vector<int> modes = segmenter.getModes();
+			
+			if (segments.size() > 0) {
+				cout << "\tSegments ([start frame]-[end frame]): " << endl;
+				
+				for (int i = 0; i < segments.size(); i++)
+					cout << "\t\t" << i << ": " << segments[i][0] << "-" << segments[i][1] << endl;
+			} else
+				cout << "\tNo segments." << endl;
+			
+			/*-----------------------------------------------------------------*
+			 * 5. Reload the sound and cut it up, saving the segments to disk. *
+			 *-----------------------------------------------------------------*/
+			cout << "5. Saving segments to disk." << endl;
+			
+			if (segments.size() < 1)
+				cout << "\tNo segments to save." << endl;
+			else {
+				cout << "\tSaving " << segments.size() << " segments." << endl;
+				
+				for (int i = 0; i < segments.size(); i++) {
+					cout << "\t\tSaving segment " << i << "." << endl;
+					sound.saveSegment("segment" + double_to_string(i) + ".wav", segments[i][0], segments[i][1]);
+				}
 			}
+			
+			/*--------------*
+			 * 6. Clean up. *
+			 *--------------*/
+			
+			sound.close();
+		} catch (exception& e) {
+			cerr << e.what() << endl;
 		}
-		
-		/*--------------*
-		 * 6. Clean up. *
-		 *--------------*/
-		
-		sound->close();
-		
-		delete sound;
-		delete segmenter;
-		delete feature_set;
-		delete loudness;
-		delete spectral_centroid;
-		delete spectral_sparsity;
-	
-		return 0;
 	}
+	
+	return 0;
 }
